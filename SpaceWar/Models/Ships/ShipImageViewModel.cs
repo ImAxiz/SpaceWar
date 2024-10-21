803b83a967a7e775989ec35a40e6c41e8543b664
ImAxiz <markovassiljev01@gmail.com>
2024-10-21 16:31:14 +0300

    Added ShipImageViewModel and did some stuff in _Image

---
+++ SpaceWar/Models/Ships/ShipImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceWar.Models.Ships
+{
+    public class ShipImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public string ImageTitle { get; set; }
+        public byte[] imageData { get; set; }
+        public string Image { get; set; }
+        public Guid? ShipID { get; set; }
+    }
+}
