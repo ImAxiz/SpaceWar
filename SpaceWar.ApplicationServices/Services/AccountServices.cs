fc95e27a024e4f382234d39dc84e09e7a383559c
ImAxiz <markovassiljev01@gmail.com>
2024-11-20 15:20:34 +0200

    Added account interface with service aswell

---
+++ SpaceWar.ApplicationServices/Services/AccountServices.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SpaceWar.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceWar.ApplicationServices.Services
+{
+    public class AccountServices : IAccountServices
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountServices
+            (
+                UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager
+            )
+            
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
